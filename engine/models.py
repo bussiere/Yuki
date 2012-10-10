9e7f77e77c7d7550b797a1308b8948b776dba4b2
bussiere <bussiere@gmail.com>
2012-10-10 16:59:20 +0200

    petits rajouts

--- engine/models.py
+++ engine/models.py
@@ -6,3 +6,4 @@ class Item(models.Model):
 	Barcode = models.ManyToManyField('barcode.BarCode',null=True,blank=True)
 	Object = models.ForeignKey('object.Object',null=True,blank=True)
 	Avis = models.ManyToManyField('avis.Avis',null=True,blank=True)
+	Demande = models.BooleanField(default=False)
--- seller/models.py
+++ seller/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-
+class ImageSeller(models.Model):
+    Image = models.CharField(max_length=200,null=True,blank=True)
+    def ___str__(self):
+        return self.Nom
+    def __unicode__(self):
+        return self.Nom
 
 class CategorieSeller(models.Model):
     Nom = models.CharField(max_length=200,null=True,blank=True)
@@ -19,3 +24,4 @@ class Seller(models.Model):
     SiteWeb = models.CharField(max_length=600,null=True,blank=True)
     Lieu = models.ForeignKey('lieu.Lieu',null=True, blank=True)
     Note_divers = models.ManyToManyField('users.Note',null=True,blank=True)
+    Image = models.ManyToManyField('ImageSeller',null=True,blank=True)
